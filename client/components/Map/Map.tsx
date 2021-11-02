f3bf053e2a41ba1bb8694aca218f20d78de69cde
Mafrans <malte.kluft@gmail.com>
2021-11-02 20:44:41 +0100

    🚧 Add Map boilerplate component

---
+++ client/components/Map/Map.tsx
@@ -0,0 +1,12 @@
+// boilerplate React.FC component
+import React from 'react'
+
+type MapProps = {}
+
+export const Map: React.FC<MapProps> = () => {
+  return (
+    <div>
+      <h1>Map</h1>
+    </div>
+  )
+}
